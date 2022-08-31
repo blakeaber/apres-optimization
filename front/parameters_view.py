16fafd8aae61f2c2d4b16d8741a96b9c5aa16377
jorgeApres <jorge@apres.io>
2022-08-31 16:48:22 +0200

    API - Updated import and parameters view

--- front/parameters_view.py
+++ front/parameters_view.py
@@ -11,6 +11,27 @@ layout = html.Div(
                     [
                         dbc.InputGroup(
                             [
+                                dbc.InputGroupText("Minutes Between Time Ticks"),
+                                dbc.Input(type="number", placeholder=15, disabled=True),
+                            ],
+                            className="mb-3",
+                        ),
+                        dbc.InputGroup(
+                            [
+                                dbc.InputGroupText("Hours to simulate"),
+                                dbc.Input(
+                                    id="hours_to_simulate",
+                                    type="number",
+                                    placeholder=24,
+                                    min=1,
+                                    max=168,
+                                    step=1,
+                                ),
+                            ],
+                            className="mb-3",
+                        ),
+                        dbc.InputGroup(
+                            [
                                 dbc.InputGroupText("Total Vehicles In Market"),
                                 dbc.Input(
                                     id="vehicle_count",
@@ -47,6 +68,21 @@ layout = html.Div(
                                     min=1,
                                     max=23,
                                     step=1,
+                                ),
+                            ],
+                            className="mb-3",
+                        ),
+                        dbc.InputGroup(
+                            [
+                                dbc.InputGroupText(
+                                    "Minimum Time Between Shifts (minutes)"
+                                ),
+                                dbc.Input(
+                                    id="min_time_between_shifts",
+                                    type="number",
+                                    placeholder=30,
+                                    min=0,
+                                    step=15,
                                 ),
                             ],
                             className="mb-3",
@@ -141,11 +177,39 @@ layout = html.Div(
                                     switch=True,
                                 ),
                                 dbc.FormText(
-                                    "Too many constraints can lead to a lack of solutions"
+                                    "Too many hard constraints can lead to a lack of solutions"
                                 ),
                             ],
                             className="mb-3",
-                        )
+                        ),
+                        dbc.InputGroup(
+                            [
+                                dbc.InputGroupText("Cost of Rush Hour soft constraint"),
+                                dbc.InputGroupText("$"),
+                                dbc.Input(
+                                    id="rush_hour_soft_constraint_cost",
+                                    type="number",
+                                    placeholder=0,
+                                    min=0,
+                                ),
+                            ],
+                            className="mb-3",
+                        ),
+                        dbc.InputGroup(
+                            [
+                                dbc.InputGroupText(
+                                    "Cost of Minimum Shifts soft constraint"
+                                ),
+                                dbc.InputGroupText("$"),
+                                dbc.Input(
+                                    id="minimum_shifts_soft_constraint_cost",
+                                    type="number",
+                                    placeholder=0,
+                                    min=0,
+                                ),
+                            ],
+                            className="mb-3",
+                        ),
                     ],
                     title="Manage Constraints",
                 ),
@@ -166,31 +230,41 @@ layout = html.Div(
 @callback(
     Output("parameter-confirmation", "children"),
     Input("parameter-submit", "n_clicks"),
+    State("hours_to_simulate", "value"),
     State("vehicle_count", "value"),
     State("min_shift", "value"),
     State("max_shift", "value"),
+    State("min_time_between_shifts", "value"),
     State("vehicle_cost", "value"),
     State("trip_revenue", "value"),
     State("depot_starts", "value"),
     State("depot_ends", "value"),
     State("constraint_switches", "value"),
+    State("rush_hour_soft_constraint_cost", "value"),
+    State("minimum_shifts_soft_constraint_cost", "value"),
 )
 def on_save_params(
     n_clicks,
+    hours_to_simulate,
     num_vehicles,
     min_shift,
     max_shift,
+    min_time_between_shifts,
     vehicle_cost,
     trip_revenue,
     depot_starts,
     depot_ends,
     constraint_flags,
+    rush_hour_soft_constraint_cost,
+    minimum_shifts_soft_constraint_cost,
 ):
     if n_clicks is not None:
         parameter_settings = {
+            "num_hours": hours_to_simulate,
             "num_vehicles": num_vehicles,
             "min_duration": min_shift,
             "max_duration": max_shift,
+            "min_time_between_shifts": min_time_between_shifts,
             "cost_vehicle_per_minute": vehicle_cost,
             "revenue_passenger": trip_revenue,
             "max_starts_per_slot": depot_starts,
@@ -198,6 +272,8 @@ def on_save_params(
             "enable_rush_hour_constraint": False,
             "enable_market_hour_constraint": False,
             "enable_min_shift_constraint": False,
+            "rush_hour_soft_constraint_cost": rush_hour_soft_constraint_cost,
+            "minimum_shifts_soft_constraint_cost": minimum_shifts_soft_constraint_cost,
         }
 
         if any([v == None for v in parameter_settings.values()]):
